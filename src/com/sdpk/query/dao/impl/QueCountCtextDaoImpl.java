f3c56d666a91f7513b522a055be0f9f8a739f481
liuxin0216 <1172144699@qq.com>
2018-01-27 19:05:05 +0800

    合同课消统计+刘鑫+0127
    Signed-off-by: liuxin0216 <1172144699@qq.com>

--- src/com/sdpk/query/dao/impl/QueCountCtextDaoImpl.java
+++ src/com/sdpk/query/dao/impl/QueCountCtextDaoImpl.java
@@ -12,6 +12,7 @@ import com.sdpk.model.QueCountCtext;
 import com.sdpk.model.Student;
 import com.sdpk.query.dao.QueCountCtextDao;
 import com.sdpk.utility.DBUtility;
+
 /*
  * @author 刘鑫
  * @date 2018-01-25 18:17
@@ -20,139 +21,156 @@ public class QueCountCtextDaoImpl implements QueCountCtextDao {
 	private Connection connection;
 	boolean dao = false;
 
-	public QueCountCtextDaoImpl(){
+	public QueCountCtextDaoImpl() {
 		System.out.println("connection对象在QueCountCtextDaoImpl连接!");
 	}
+
 	@Override
 	public ArrayList<QueCountCtext> queryCountCtext(String uuid) {
 		// TODO Auto-generated method stub
-		ArrayList<QueCountCtext> ctextList=new ArrayList<QueCountCtext>();
+		ArrayList<QueCountCtext> ctextList = new ArrayList<QueCountCtext>();
 		Statement statement1 = null;
-		Statement statement2=null;
-		Statement statement3=null;
-		Statement statement4=null;
-		
+		Statement statement2 = null;
+		Statement statement3 = null;
+		Statement statement4 = null;
+
 		ResultSet rs1 = null;
 		ResultSet rs2 = null;
 		ResultSet rs3 = null;
 		ResultSet rs4 = null;
-		try{
+		try {
 			connection = DBUtility.open();
-			QueCountCtext qcc=new QueCountCtext();
-			int rowcount=0;
-			String rowcountt="";
-			int kesum=0;
-			String kesumm="";
-			int kebefore=0;
-			String kebeforee="";
-			int keafter=0;
-			String keafterr="";
-			//获取当前时间
-			Date date=new Date();
-			SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MMdd");
-			
+			QueCountCtext qcc = new QueCountCtext();
+			int rowcount = 0;
+			String rowcountt = "";
+			int kesum = 0;
+			String kesumm = "";
+			int kebefore = 0;
+			String kebeforee = "";
+			int keafter = 0;
+			String keafterr = "";
+			// 获取当前时间
+			Date date = new Date();
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MMdd");
+
 			statement1 = connection.createStatement();
-			statement2=connection.createStatement();
-			statement3=connection.createStatement();
-			statement4=connection.createStatement();
-			
-			//根据单个学生id查询所对应的几个班级，几个班级所对应的几个合同
-			rs1=statement1.executeQuery("select DISTINCT tcc.* from t_class_contract tcc,t_contrtext tc where tcc.classUuid in(select tcs.classUuid from t_class_stu tcs where tcs.stuUuid='"+uuid+"') and tc.openAndclose='open';");
-			//根据学生id查询所对应的几个班级，几个班级所对应的几节课
-			rs2=statement2.executeQuery("select tpa.* from t_paike_all tpa where tpa.claUuid in(select tcs.classUuid from t_class_stu tcs where tcs.stuUuid='"+uuid+"')");
-			//根据学生id查询学生姓名和id
-			rs3=statement3.executeQuery("select * from t_student where uuid='"+uuid+"';");
-			//根据学生id查询合同表中学生对应总课数
-			rs4=statement4.executeQuery("select sum(totalCount) from t_contrtext where stuUuid='"+uuid+"' and openAndclose='open';");
-			
-				while(rs1.next()){
-					rowcount++;
-					rowcountt=rowcount+"";
-					//合同数量
-					qcc.setcTextCount(rowcountt);
-				}
-			
-				while(rs2.next()){
-					//已排课
-					kesum++;
-					kesumm=kesum+"";
-					qcc.setPaikeSum(kesumm);
-					//已上过的课
-					if(sdf.parse(rs2.getString("keDateTime")).before(date)){
-						kebefore++;
-						//System.out.println("上的课==="+kebefore);
-						kebeforee=kebefore+"";
-						qcc.setKeDayBefore(kebeforee);
-						/*System.out.println("排课时间=========="+sdf.parse(rs2.getString("keDateTime")));
-						System.out.println("当前时间=========="+date);
-						System.out.println("已上课次数============"+kebefore);
-						System.out.println("排课时间是否在当前时间之前============"+sdf.parse(rs2.getString("keDateTime")).before(date));*/
-					}
-					//没上过的课
-					keafter=kesum-kebefore;
-					keafterr=keafter+"";
-					
-					qcc.setKeDayAfter(keafterr);
-					
-					
-				}
-			
-			//ctextList.add(qcc);
-			while(rs3.next()){
-				//学生名字
-				qcc.setStuName(rs3.getString("name"));
-				//学生id
-				qcc.setStuUuid(rs3.getString("uuid"));
-				
+			statement2 = connection.createStatement();
+			statement3 = connection.createStatement();
+			statement4 = connection.createStatement();
+
+			// 根据单个学生id查询所对应的几个班级，几个班级所对应的几个合同
+			rs1 = statement1
+					.executeQuery("select DISTINCT tcc.* from t_class_contract tcc,t_contrtext tc where tcc.classUuid in(select tcs.classUuid from t_class_stu tcs where tcs.stuUuid='"
+							+ uuid + "') and tc.openAndclose='open';");
+			// 根据学生id查询所对应的几个班级，几个班级所对应的几节课
+			rs2 = statement2
+					.executeQuery("select tpa.* from t_paike_all tpa where tpa.claUuid in(select tcs.classUuid from t_class_stu tcs where tcs.stuUuid='"
+							+ uuid + "')");
+			// 根据学生id查询学生姓名和id
+			rs3 = statement3
+					.executeQuery("select * from t_student where uuid='" + uuid
+							+ "';");
+			// 根据学生id查询合同表中学生对应总课数
+			rs4 = statement4
+					.executeQuery("select sum(totalCount) from t_contrtext where stuUuid='"
+							+ uuid + "' and openAndclose='open';");
+
+			while (rs1.next()) {
+				rowcount++;
+				rowcountt = rowcount + "";
+				// 合同数量
+				qcc.setcTextCount(rowcountt);
 			}
-			
-				while(rs4.next()){
-					qcc.setKeTotal(rs4.getString("sum(totalCount)"));
-					String keTotalstr=rs4.getString("sum(totalCount)");
 
-						try {
-						    int keTotal_int = Integer.valueOf(keTotalstr).intValue();
-						  //合同剩余次数
-							int cMoreThan=keTotal_int-Integer.valueOf(qcc.getKeDayBefore()).intValue();
-							String cMoreThann=cMoreThan+"";
-							qcc.setcMoreThan(cMoreThann);
-						} catch (NumberFormatException e) {
-						    e.printStackTrace();
-						}
-
-					
+			while (rs2.next()) {
+				// 已排课
+				kesum++;
+				kesumm = kesum + "";
+				qcc.setPaikeSum(kesumm);
+				// 已上过的课
+				if (sdf.parse(rs2.getString("keDateTime")).getTime() > date
+						.getTime()) {
+					kebefore++;
+					// System.out.println("上的课==="+kebefore);
+					kebeforee = kebefore + "";
+					qcc.setKeDayBefore(kebeforee);
+					// System.out.println(sdf.parse(rs2.getString("keDateTime")).getTime());
+					// System.out.println(date.getTime());
+					// System.out.println(sdf.parse(rs2.getString("keDateTime")).getTime()<date.getTime());
+					/*
+					 * System.out.println("排课时间=========="+sdf.parse(rs2.getString
+					 * ("keDateTime")));
+					 * System.out.println("当前时间=========="+date);
+					 * System.out.println("已上课次数============"+kebefore);
+					 * System.out
+					 * .println("排课时间是否在当前时间之前============"+sdf.parse(rs2
+					 * .getString("keDateTime")).before(date));
+					 */
 				}
-				
-			
+				// 没上过的课
+				keafter = kesum - kebefore;
+				keafterr = keafter + "";
+
+				qcc.setKeDayAfter(keafterr);
+
+			}
+
+			// ctextList.add(qcc);
+			while (rs3.next()) {
+				// 学生名字
+				qcc.setStuName(rs3.getString("name"));
+				// 学生id
+				qcc.setStuUuid(rs3.getString("uuid"));
+
+			}
+
+			while (rs4.next()) {
+				qcc.setKeTotal(rs4.getString("sum(totalCount)"));
+				String keTotalstr = rs4.getString("sum(totalCount)");
+
+				try {
+					int keTotal_int = Integer.valueOf(keTotalstr).intValue();
+					// 合同剩余次数
+					int cMoreThan = keTotal_int
+							- Integer.valueOf(qcc.getKeDayBefore()).intValue();
+					String cMoreThann = cMoreThan + "";
+					qcc.setcMoreThan(cMoreThann);
+				} catch (NumberFormatException e) {
+					e.printStackTrace();
+				}
+
+			}
+
 			ctextList.add(qcc);
-			
-			System.out.println("合同数量==============="+qcc.getcTextCount());
-			System.out.println("合同已排课==============="+qcc.getPaikeSum());
-			System.out.println("已上过的课==============="+qcc.getKeDayBefore());
-			System.out.println("没上过的课==============="+qcc.getKeDayAfter());
-			System.out.println("学生名字==============="+qcc.getStuName());
-			System.out.println("学生id==============="+qcc.getStuUuid());
-			System.out.println("合同总课数==============="+qcc.getKeTotal());
-			System.out.println("合同剩余课数==============="+qcc.getcMoreThan());
-		}catch(Exception e){
+
+			System.out.println("合同数量===============" + qcc.getcTextCount());
+			System.out.println("合同已排课===============" + qcc.getPaikeSum());
+			System.out.println("已上过的课===============" + qcc.getKeDayBefore());
+			System.out.println("没上过的课===============" + qcc.getKeDayAfter());
+			System.out.println("学生名字===============" + qcc.getStuName());
+			System.out.println("学生id===============" + qcc.getStuUuid());
+			System.out.println("合同总课数===============" + qcc.getKeTotal());
+			System.out.println("合同剩余课数===============" + qcc.getcMoreThan());
+		} catch (Exception e) {
 			e.printStackTrace();
 			System.out.println("MonthHeadDaoImpl的queryMonthKeByClaUuid查询失败");
-		}
-		finally{
+		} finally {
 			DBUtility.close(rs1, statement1, connection);
 			DBUtility.close(rs2, statement2, connection);
 			DBUtility.close(rs3, statement3, connection);
 			DBUtility.close(rs4, statement4, connection);
 		}
 		for (QueCountCtext queCountCtext : ctextList) {
-			System.out.println(queCountCtext.getStuName()+"============"+queCountCtext.getStuUuid());
+			System.out.println(queCountCtext.getStuName() + "============"
+					+ queCountCtext.getStuUuid());
 		}
 		return ctextList;
 	}
+
 	@Override
 	public ArrayList<String> queryAllstuUuid() {
 		// TODO Auto-generated method stub
-		
+
 		ArrayList<String> studentList = new ArrayList<String>();
 		connection = DBUtility.open();// 打开数据库连接
 		Statement statement = null;// finally关闭数据库连接
@@ -163,8 +181,8 @@ public class QueCountCtextDaoImpl implements QueCountCtextDao {
 			rs = statement.executeQuery("select uuid from t_student");
 			while (rs.next()) {
 
-				String stuUuid=rs.getString("uuid");
-				
+				String stuUuid = rs.getString("uuid");
+
 				studentList.add(stuUuid);
 			}
 		} catch (SQLException e) {
@@ -179,6 +197,5 @@ public class QueCountCtextDaoImpl implements QueCountCtextDao {
 
 		return studentList;
 	}
-
 
 }
