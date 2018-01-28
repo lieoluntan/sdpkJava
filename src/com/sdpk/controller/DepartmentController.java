1ca07810cfc0053392c73292270cab190f83e2f4
liuxin0216 <1172144699@qq.com>
2018-01-28 19:43:32 +0800

    post请求为空修改+刘鑫+0128
    Signed-off-by: liuxin0216 <1172144699@qq.com>

--- src/com/sdpk/controller/DepartmentController.java
+++ src/com/sdpk/controller/DepartmentController.java
@@ -71,38 +71,68 @@ public class DepartmentController extends HttpServlet {
 
 			} else {
 				System.out.println("前台传入post请求体数据为空，请联系管理员！");
-
+				backResult.setMessage("信息值：失败");
+				backResult.setQingqiu("前台传入post请求体数据为空，请联系管理员！");
 			}
 
 		} else if (qqiu.equals("delete")) {
 			T_DataControl t_data = new T_DataControl();
 			String str = t_data.getRequestPayload(request);
-			Map<String, Object> map = t_data.JsonStrToMap(str);
-			System.out.println("前台传来的uuid=================="
-					+ (String) map.get("uuid"));
-			dts.deleteDepartment((String) map.get("uuid"));
-			backResult.setMessage("信息值：成功");
-			backResult.setQingqiu("delete删除");
-			ArrayList<String> list = new ArrayList<String>();
-			list.add((String) map.get("uuid"));
-			backResult.setData(list);
+			if (str != null && str != "" && str.length() != 0) { // 非空判断，防止前台传空报500服务器错误中的空指针
+				Map<String, Object> map = t_data.JsonStrToMap(str);
+				T_DataMap2Bean t_map2bean = new T_DataMap2Bean();
+
+				System.out.println("前台传来的uuid=================="
+						+ (String) map.get("uuid"));
+				dts.deleteDepartment((String) map.get("uuid"));
+				backResult.setMessage("信息值：成功");
+				backResult.setQingqiu("delete删除");
+				ArrayList<String> list = new ArrayList<String>();
+				list.add((String) map.get("uuid"));
+				backResult.setData(list);
+			}else{
+				System.out.println("前台传入post请求体数据为空，请联系管理员！");
+				backResult.setMessage("信息值：失败");
+				backResult.setQingqiu("前台传入post请求体数据为空，请联系管理员！");
+			}
+			
 		} else if (qqiu.equals("edit")) {
 			T_DataControl t_data = new T_DataControl();
 			String str = t_data.getRequestPayload(request);
-			Department department = new Department();
-			T_DataMap2Bean t_map2bean = new T_DataMap2Bean();
-			Map<String, Object> map = t_data.JsonStrToMap(str);
-			department = t_map2bean.MapToDepartment(map);
-			department.setUuid((String) map.get("uuid"));
-			System.out.println("前台传来的uuid=================="
-					+ department.getUuid());
-			dts.updateDepartment(department);
-			backResult.setMessage("信息值：成功");
-			backResult.setQingqiu("update修改");
-			ArrayList<String> list = new ArrayList<String>();
-			list.add(department.getUuid());
-			backResult.setData(list);
+			if (str != null && str != "" && str.length() != 0) { // 非空判断，防止前台传空报500服务器错误中的空指针
+				Map<String, Object> map = t_data.JsonStrToMap(str);
+				T_DataMap2Bean t_map2bean = new T_DataMap2Bean();
+				Department department = new Department();
+				department = t_map2bean.MapToDepartment(map);
+				department.setUuid((String) map.get("uuid"));
+				System.out.println("前台传来的uuid=================="
+						+ department.getUuid());
+				dts.updateDepartment(department);
+				backResult.setMessage("信息值：成功");
+				backResult.setQingqiu("update修改");
+				ArrayList<String> list = new ArrayList<String>();
+				list.add(department.getUuid());
+				backResult.setData(list);
+			}else{
+				System.out.println("前台传入post请求体数据为空，请联系管理员！");
+				backResult.setMessage("信息值：失败");
+				backResult.setQingqiu("前台传入post请求体数据为空，请联系管理员！");
+			}
+			
 		} else if (qqiu.equals("list")) {
+			T_DataControl t_data = new T_DataControl();
+			String str = t_data.getRequestPayload(request);
+			if (str != null && str != "" && str.length() != 0) { // 非空判断，防止前台传空报500服务器错误中的空指针
+				Map<String, Object> map = t_data.JsonStrToMap(str);
+				ArrayList<Department> list = dts.listDepartment();
+				backResult.setMessage("信息值：成功");
+				backResult.setQingqiu("list查询列表");
+				backResult.setData(list);
+			}else{
+				System.out.println("前台传入post请求体数据为空，请联系管理员！");
+				backResult.setMessage("信息值：失败");
+				backResult.setQingqiu("前台传入post请求体数据为空，请联系管理员！");
+			}
 			ArrayList<Department> list = dts.listDepartment();
 			backResult.setMessage("信息值：成功");
 			backResult.setQingqiu("list查询列表");
@@ -111,6 +141,21 @@ public class DepartmentController extends HttpServlet {
 			T_DataControl t_data = new T_DataControl();
 			String str = t_data.getRequestPayload(request);
 			Map<String, Object> map = t_data.JsonStrToMap(str);
+			if (str != null && str != "" && str.length() != 0) { // 非空判断，防止前台传空报500服务器错误中的空指针
+				System.out.println("前台传来的uuid=================="
+						+ (String) map.get("uuid"));
+				Department department = dts.serachOneDepartment((String) map
+						.get("uuid"));
+				backResult.setMessage("信息值：成功");
+				backResult.setQingqiu("getOne查询单条记录");
+				ArrayList<Department> list = new ArrayList<Department>();
+				list.add(department);
+				backResult.setData(list);
+			}else{
+				System.out.println("前台传入post请求体数据为空，请联系管理员！");
+				backResult.setMessage("信息值：失败");
+				backResult.setQingqiu("前台传入post请求体数据为空，请联系管理员！");
+			}
 			System.out.println("前台传来的uuid=================="
 					+ (String) map.get("uuid"));
 			Department department = dts.serachOneDepartment((String) map
@@ -124,20 +169,30 @@ public class DepartmentController extends HttpServlet {
 			T_DataControl t_data = new T_DataControl();
 			String str = t_data.getRequestPayload(request);
 			Map<String, Object> map = t_data.JsonStrToMap(str);
-			System.out.println("前台传来的uuid=================="
-					+ (String) map.get("uuid"));
-			if (map.get("openAndclose").equals("open")) {
-				dts.updateOnOff((String) map.get("uuid"), "open");
-				backResult.setMessage("操作成功：已打开");
-				backResult.setQingqiu("on");
-			} else if (map.get("openAndclose").equals("close")) {
-				dts.updateOnOff((String) map.get("uuid"), "close");
-				backResult.setMessage("操作成功：已关闭");
-				backResult.setQingqiu("off");
-			} else {
-				backResult.setMessage("操作失败：开关参数不规范(close)，联系前端开发");
-				backResult.setQingqiu("err");
+			if (str != null && str != "" && str.length() != 0) {
+				System.out.println("前台传来的uuid=================="
+						+ (String) map.get("uuid"));
+				if (map.get("openAndclose").equals("open")) {
+					dts.updateOnOff((String) map.get("uuid"), "open");
+					backResult.setMessage("操作成功：已打开");
+					backResult.setQingqiu("on");
+					backResult.setData(null);
+				} else if (map.get("openAndclose").equals("close")) {
+					dts.updateOnOff((String) map.get("uuid"), "close");
+					backResult.setMessage("操作成功：已关闭");
+					backResult.setQingqiu("off");
+					backResult.setData(null);
+				} else {
+					backResult.setMessage("操作失败：开关参数不规范(close)，联系前端开发");
+					backResult.setQingqiu("err");
+					backResult.setData(null);
+				}
+			}else{
+				System.out.println("前台传入post请求体数据为空，请联系管理员！");
+				backResult.setMessage("信息值：失败");
+				backResult.setQingqiu("前台传入post请求体数据为空，请联系管理员！");
 			}
+			
 
 		} else {
 			System.out.println("qqiu请求参数  " + qqiu + "  不规范");
