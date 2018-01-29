c56f723ce522bb0d6f323f986b4aafe4a2cade29
liuxin0216 <1172144699@qq.com>
2018-01-29 20:12:14 +0800

    修改+刘鑫+0129
    Signed-off-by: liuxin0216 <1172144699@qq.com>

--- src/com/sdpk/service/impl/ClassRoomServiceImpl.java
+++ src/com/sdpk/service/impl/ClassRoomServiceImpl.java
@@ -13,105 +13,102 @@ import com.sdpk.query.nameQuery.dao.impl.NameReCRoomDaoImpl;
 import com.sdpk.service.ClassRoomService;
 
 /**
- *树袋老师
+ * 树袋老师
+ * 
  * @author 作者 xpp
- * @version 创建时间：2017-10-30 上午9:46:29
- * 类说明
+ * @version 创建时间：2017-10-30 上午9:46:29 类说明
  */
 
-public class ClassRoomServiceImpl implements ClassRoomService{
-	private NameReCRoomDao nameReCRoomDao=new NameReCRoomDaoImpl();
-  private ClassRoomDao classRoomDao= new ClassRoomDaoImpl();
+public class ClassRoomServiceImpl implements ClassRoomService {
+	private NameReCRoomDao nameReCRoomDao = new NameReCRoomDaoImpl();
+	private ClassRoomDao classRoomDao = new ClassRoomDaoImpl();
 
-  @Override
-  public String insert(ClassRoom classRoom) {
-    // TODO Auto-generated method stub
-	  String flag = this.getClaSSRoomByName1(classRoom);
-	  if(flag.equals("yes")){
-		  return flag;
-	  }else{
-	  classRoom.setUuid(null);
+	@Override
+	public String insert(ClassRoom classRoom) {
+		// TODO Auto-generated method stub
+		String flag = this.getClaSSRoomByName1(classRoom);
+		if (flag.equals("yes")) {
+			return flag;
+		} else {
+			classRoom.setUuid(null);
 
-    classRoom.setUuid(UUID.randomUUID().toString());
-    System.out.println("^^在ClassRoomServiceImpl收到数据 ："+classRoom.toString()+"收到结束!");
-    boolean daoFlag = classRoomDao.insert(classRoom);
-    if(daoFlag)
-    {
-    return classRoom.getUuid();
-    }else{
-      return "插入不成功,dao层执行有出错地方,请联系管理员";
-    	   }	
-	  }
-  }//end method insert
+			classRoom.setUuid(UUID.randomUUID().toString());
+			System.out.println("^^在ClassRoomServiceImpl收到数据 ："
+					+ classRoom.toString() + "收到结束!");
+			boolean daoFlag = classRoomDao.insert(classRoom);
+			if (daoFlag) {
+				return classRoom.getUuid();
+			} else {
+				return "插入不成功,dao层执行有出错地方,请联系管理员";
+			}
+		}
+	}// end method insert
 
-  @Override
-  public String delete(String uuid) {
-    // TODO Auto-generated method stub
-    if(uuid!=null&&uuid!="")
-    {
-      boolean daoFlag = classRoomDao.delete(uuid);
-      
-        if(daoFlag)
-        {
-        return uuid;
-        }else{
-          return "删除不成功,dao层执行有出错地方,请联系管理员";
-        }
-    }else{
-      String msg="ClassRoomServiceImpl delete方法中的uuid为空，或格式不正确，请重新选择";
-      System.out.println(msg);
-      return msg;
-    }
-    
-  }//end method delete
+	@Override
+	public String delete(String uuid) {
+		// TODO Auto-generated method stub
+		if (uuid != null && uuid != "") {
+			boolean daoFlag = classRoomDao.delete(uuid);
 
-  @Override
-  public String update(ClassRoom classRoom) {
-    // TODO Auto-generated method stub
-	  String flag = this.getClaSSRoomByName1(classRoom);
-	  if(flag.equals("yes")){
-		  return flag;
-	  }else{
-	String uuid = classRoom.getUuid();
-    if(uuid!=null&&uuid!="")
-    {
-      boolean daoFlag = classRoomDao.update(classRoom);
-        if(daoFlag)
-        {
-        return uuid;
-        }else{
-          return "修改不成功,dao层执行有出错地方,请联系管理员";
-        }
-    }else{
-      String msg="ClassRoomServiceImpl update方法中的uuid为空，或格式不正确，请重新选择";
-      System.out.println(msg);
-      return msg;
-    		}
-      }//end method update
-}
-  @Override
-  public ClassRoom getByUuid(String uuid) {
-    // TODO Auto-generated method stub
-    if(uuid!=null&&uuid!="")
-    {
-      ClassRoom classRoom = classRoomDao.getByUuid(uuid);
-    return classRoom;
-    }else{
-      System.out.println("ContractServiceImpl getByUuid方法中的uuid为空，或格式不正确，请联系管理员");
-      ClassRoom classRoomX= new ClassRoom();
-    return classRoomX;
-    }
-  }//end method getByUuid
+			if (daoFlag) {
+				return uuid;
+			} else {
+				return "删除不成功,dao层执行有出错地方,请联系管理员";
+			}
+		} else {
+			String msg = "ClassRoomServiceImpl delete方法中的uuid为空，或格式不正确，请重新选择";
+			System.out.println(msg);
+			return msg;
+		}
 
-  @Override
-  public ArrayList<ClassRoom> getList() {
-    // TODO Auto-generated method stub
-    ArrayList<ClassRoom> classRoomlist = classRoomDao.getList();
-    
-    return classRoomlist;
-  }//end method getList()
+	}// end method delete
 
-  @Override
+	@Override
+	public String update(ClassRoom classRoom) {
+		// TODO Auto-generated method stub
+		String flag = this.getClaSSRoomByName1(classRoom);
+		if (flag.equals("yes")) {
+			return flag;
+		} else {
+			String uuid = classRoom.getUuid();
+			if (uuid != null && uuid != "") {
+				boolean daoFlag = classRoomDao.update(classRoom);
+				if (daoFlag) {
+					return uuid;
+				} else {
+					return "修改不成功,dao层执行有出错地方,请联系管理员";
+				}
+			} else {
+				String msg = "ClassRoomServiceImpl update方法中的uuid为空，或格式不正确，请重新选择";
+				System.out.println(msg);
+				return msg;
+			}
+		}// end method update
+	}
+
+	@Override
+	public ClassRoom getByUuid(String uuid) {
+		// TODO Auto-generated method stub
+		if (uuid != null && uuid != "") {
+			ClassRoom classRoom = classRoomDao.getByUuid(uuid);
+			return classRoom;
+		} else {
+			System.out
+					.println("ContractServiceImpl getByUuid方法中的uuid为空，或格式不正确，请联系管理员");
+			ClassRoom classRoomX = new ClassRoom();
+			return classRoomX;
+		}
+	}// end method getByUuid
+
+	@Override
+	public ArrayList<ClassRoom> getList() {
+		// TODO Auto-generated method stub
+		ArrayList<ClassRoom> classRoomlist = classRoomDao.getList();
+
+		return classRoomlist;
+	}// end method getList()
+
+	@Override
 	public String getClassRoomByName(ClassRoom cR) {
 		// TODO Auto-generated method stub
 		String flag = "";
@@ -137,43 +134,41 @@ public class ClassRoomServiceImpl implements ClassRoomService{
 
 		List<ClassRoom> crList = nameReCRoomDao.getCRByName(CR);
 		for (ClassRoom cr2 : crList) {
-			//编辑验证重名要过滤掉自己本身的名字
+			// 编辑验证重名要过滤掉自己本身的名字
 			String s2Uuid = cr2.getUuid();
 			boolean flagSelf = s2Uuid.equals(CR.getUuid());
 			boolean flagNotSelf = !flagSelf;
-			if(flagNotSelf){//编辑验证重名要过滤
+			if (flagNotSelf) {// 编辑验证重名要过滤
 
-			if (cr2.getUuid() != null) {
-				flag = "yes";
+				if (cr2.getUuid() != null) {
+					flag = "yes";
 
-				return flag;
-			}
-		  }//end if(flagNotSelf)
+					return flag;
+				}
+			}// end if(flagNotSelf)
 		}
 		flag = "no";
 		return flag;
-	}//end method
+	}// end method
 
 	@Override
 	public String getonoff(ClassRoom classRoom) {
 		// TODO Auto-generated method stub
 		String uuid = classRoom.getUuid();
-		if(uuid!=null&&uuid!="")
-	    {
-		  String oAc = classRoom.getOpenAndclose();
-	      boolean daoFlag = classRoomDao.updateOnOff(uuid,oAc);
-	      
-	        if(daoFlag)
-	        {
-	        return "操作成功";
-	        }else{
-	          return "操作失败,dao层执行有出错地方,请联系管理员";
-	        }
-	    }else{
-	      String msg="ClassRoomServiceImpl delete方法中的uuid为空，或格式不正确，请重新选择";
-	      System.out.println(msg);
-	      return msg;
-	    }
-	}//end method
+		if (uuid != null && uuid != "") {
+			String oAc = classRoom.getOpenAndclose();
+			boolean daoFlag = classRoomDao.updateOnOff(uuid, oAc);
 
-}//end class 
+			if (daoFlag) {
+				return "操作成功";
+			} else {
+				return "操作失败,dao层执行有出错地方,请联系管理员";
+			}
+		} else {
+			String msg = "ClassRoomServiceImpl delete方法中的uuid为空，或格式不正确，请重新选择";
+			System.out.println(msg);
+			return msg;
+		}
+	}// end method
+
+}// end class
